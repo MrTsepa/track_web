4fc855c1653d1d49ffabfb164ed2a44571761a52
mrtsepa <tsepa.stas@gmail.com>
2016-11-02 23:31:58 +0300

    detail and creation view

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from views import TaskList, TaskDetail
+from views import TaskList, TaskDetail, TaskDetailAndCreate
 
 urlpatterns = [
-        url(r'^(?P<pk>[0-9]+)$', TaskDetail.as_view(), name='task_detail'),
-        url(r'$', TaskList.as_view(), name='task_list'),
+    url(r'^(?P<pk>[0-9]+)$', TaskDetailAndCreate.as_view(), name='task_detail_and_create'),
+    # url(r'^(?P<pk>[0-9]+)$', TaskDetail.as_view(), name='task_detail'),
+    url(r'$', TaskList.as_view(), name='task_list'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.apps import apps
+from django.shortcuts import get_object_or_404, resolve_url
+from django.views.generic.edit import CreateView
+
 from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
@@ -14,3 +17,26 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     template_name = 'tasks/task_detail.html'
     model = Task
+
+
+class TaskDetailAndCreate(CreateView):
+    model = apps.get_model('solutions.Solution')
+    template_name = 'tasks/task_detail_and_create.html'
+    fields = ('code', )
+
+    def dispatch(self, request, pk=None, *args, **kwargs):
+        self.task = get_object_or_404(Task, id=pk)
+        return super(TaskDetailAndCreate, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskDetailAndCreate, self).get_context_data(**kwargs)
+        context['task'] = self.task
+        return context
+
+    def get_success_url(self):
+        return resolve_url('tasks:task_detail_and_create', pk=self.object.task.id)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.task = self.task
+        return super(TaskDetailAndCreate, self).form_valid(form)
