50c8c55cd9806f155b415de27f010f59c1b46cf3
MrTsepa <tsepa.stas@gmail.com>
2016-11-04 23:21:50 +0300

    added code detail

--- src/solutions/views.py
+++ src/solutions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import resolve_url
+from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
 from .models import Solution
 
@@ -13,3 +14,8 @@ class SolutionCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(SolutionCreate, self).form_valid(form)
+
+
+class CodeDetailView(DetailView):
+    model = Solution
+    template_name = "solutions/code_detail.html"
