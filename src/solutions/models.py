bd55c5326423205bdb3d0eebee40c1ee15c68011
mrtsepa <tsepa.stas@gmail.com>
2016-11-06 17:57:46 +0300

    AUTH_USER_MODEL

--- src/solutions/models.py
+++ src/solutions/models.py
@@ -1,13 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 
 class Solution(models.Model):
     task = models.ForeignKey('tasks.Task', related_name='solutions')
-    user = models.ForeignKey('core.User')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     code = models.TextField()
     status = models.CharField(max_length=256)
     time = models.DateTimeField(auto_now_add=True)
